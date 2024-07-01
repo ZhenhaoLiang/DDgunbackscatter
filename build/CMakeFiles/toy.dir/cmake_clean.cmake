d05a669c3541c8d18b4c39280b7d8199cc639b23
zhenhaoliang <2941526315@qq.com>
2024-07-01 00:23:28 -0700

    delete build

--- build/CMakeFiles/toy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/toy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/toy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
